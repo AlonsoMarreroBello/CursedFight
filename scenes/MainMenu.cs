868076a7f27bb7b08874fce4272bbff06ca4117a
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2025-02-23 23:46:34 +0000

    feat(MainMenu): added logic for navigation to StageSelector scene for sigleplayer and multiplayer buttons.

--- scenes/MainMenu.cs
+++ scenes/MainMenu.cs
@@ -11,4 +11,32 @@ public partial class MainMenu : Node2D
 	{
 		GetTree().Quit();
 	}
+
+	private void _on_singleplayer_button_pressed()
+	{
+		PackedScene _new_scene = GD.Load<PackedScene>("res://scenes/StageSelection.tscn");
+		if (_new_scene == null)
+		{
+			GD.Print("Error: No se pudo cargar la escena en la ruta: res://scenes/StageSelection");
+			return;
+		}
+
+		GetTree().ChangeSceneToPacked(_new_scene);
+
+	}
+
+	private void _on_multiplayer_button_pressed()
+	{
+		PackedScene _new_scene = GD.Load<PackedScene>("res://scenes/StageSelection.tscn");
+		if (_new_scene == null)
+		{
+			GD.Print("Error: No se pudo cargar la escena en la ruta: res://scenes/StageSelection");
+			return;
+		}
+
+		GetTree().ChangeSceneToPacked(_new_scene);
+
+	}
+
+
 }
