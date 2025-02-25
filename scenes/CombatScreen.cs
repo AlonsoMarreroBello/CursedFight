a58bf69b9df7993f1825fe0afc210130412da816
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2025-02-25 10:58:54 +0000

    feat(CombatScreen): added audio stream player to scene for background music and added music for each map.

--- scenes/CombatScreen.cs
+++ scenes/CombatScreen.cs
@@ -10,21 +10,29 @@ public partial class CombatScreen : Node2D
 	public override void _Ready()
 	{
 		AnimatedSprite2D _background_image = GetNode<AnimatedSprite2D>("BackgroundImage");
+		AudioStreamPlayer _audio_player = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+		AudioStream _new_audio = GD.Load<AudioStream>("res://assets/Music/the-god-beyond-the-man-kaledon-main.mp3"); ;
 		switch (Global._actual_stage)
 		{
 			case "airplane":
 				_change_texture("airplane", _background_image);
+				_new_audio = GD.Load<AudioStream>("res://assets/Music/the-god-beyond-the-man-kaledon-main.mp3");
 				break;
 			case "fight_club":
 				_change_texture("fight_club", _background_image);
+				_new_audio = GD.Load<AudioStream>("res://assets/Music/pounding-industrial-techno-instrumental.mp3");
 				break;
 			case "throne_room":
 				_change_texture("throne_room", _background_image);
+				_new_audio = GD.Load<AudioStream>("res://assets/Music/surprise-impact.mp3");
 				break;
 
 			default:
 				break;
 		}
+
+		_audio_player.Stream = _new_audio;
+		_audio_player.Play();
 
 		_timer = GetNode<Timer>("Timer");
 		_timer_label = GetNode<Label>("UI/Timer/TimerLabel");
